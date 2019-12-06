8e8682996ba216d28039606f4b930f7a7f82c81b
Oleg Arbuzov <boozze@mail.ru>
2019-12-07 02:02:19 +0300

    правки

--- build/js/main.js
+++ build/js/main.js
@@ -12,6 +12,14 @@ let footerContacts = document.querySelector('.footer__menu--contacts');
 footerContacts.classList.remove('footer__menu--nojs');
 footerNav.classList.remove('footer__menu--nojs');
 
+let accordeon = (menuToOpen, menuToClose) => {
+  let opened = 'footer__menu--opened';
+  let closed = 'footer__menu--closed';
+  if (menuToOpen.classList.contains(opened) && menuToClose.classList.contains(opened)) {
+    menuToClose.classList.remove(opened);
+    menuToClose.classList.add(closed);
+  }
+}
 
 let toggleMenu = (menu) => {
   menu.classList.toggle('footer__menu--closed');
@@ -27,18 +35,27 @@ let toggleButtonHandler = () => {
   if (footerNav && footerNavBtn) {
     footerNavBtn.addEventListener('click', () => {
       toggleMenu(footerNav);
+      accordeon(footerNav, footerContacts);
     });
   }
 
   if (footerContacts && footerContactsBtn) {
     footerContactsBtn.addEventListener('click', () => {
       toggleMenu(footerContacts);
+      accordeon(footerContacts, footerNav);
     });
+  }
+}
+
+function queryMatchHandler(mediaQuery) {
+  if (mediaQuery.matches) {
+    toggleButtonHandler();
   }
 }
 
 if (matchMedia) {
   const mediaQuery = window.matchMedia(mobileScreen);
+  queryMatchHandler(mediaQuery);
   mediaQuery.addListener(toggleButtonHandler);
 }
 
@@ -126,3 +143,35 @@ if (form) {
     form.submit();
   });
 }
+
+// плавный скролл
+
+let scrollTo = () => {
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
+      link.addEventListener('click', scrollAnchors);
+    }
+  }
+}
+
+let scrollAnchors = (e, respond = null) => {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  e.preventDefault();
+  var targetID = (respond) ? respond.getAttribute('href') : e.target.getAttribute('href');
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+  const checkIfDone = setInterval(function () {
+    const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = '-1';
+      window.history.pushState('', '', targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
+
+scrollTo();
